6326387eda5ad41fcc396cd1f9e8c6bfaa8f2290
Dane Dixon <dane@Danes-MacBook-Pro.local>
2020-06-28 20:38:34 -0400

    Committing Weather Bot

---
+++ config.js
@@ -0,0 +1,7 @@
+
+module.exports = {
+  consumer_key:         'wfaYltI9KKzAwW32BaWM7PYAG',
+  consumer_secret:      'TsOx1Tg9aSYI949yYDO6IwIx1pUYfkb3SbG6tWZ28vTjXO60mj',
+  access_token:         '1277381817146974210-vEo7w24Xtqy0pVmrIMONKNfvFoCg43',
+  access_token_secret:  'vvU0OgEwExVoWqXDgk5uDvksGnw6v4eFQhxidfrQTN4jj',
+}
---
+++ index.js
@@ -0,0 +1,21 @@
+
+var twitter = require('twit');
+var weather = require('weather-js');
+var config = require('./config');
+
+var t = new twitter(config);
+
+var tweetCurrentWeather = function() {
+  weather.find({search: 'Hungtingtown, MD', degreeType: 'F'}, function(err, result) {
+    if(err) console.log(err);
+    curr = result[0].current.temperature;
+    skytxt = result[0].current.skytext;
+    var str = "It is " + curr + " degrees Fahrenheit and " + skytxt + " in Hungtingtown, MD";
+    t.post('statuses/update', { status: str }, function(err, data, response) {
+      console.log('Tweeted!');
+    })
+  });
+}
+
+//setInterval(tweetCurrentWeather, 14400000);
+tweetCurrentWeather();
