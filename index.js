dc4186d7f650b3734ddca87b92a7e97f155c83ad
Dane Dixon <dane@Danes-MacBook-Pro.local>
2020-06-30 12:13:31 -0400

    New Account Access Keys

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var t = new twitter(config);
 
 var tweetCurrentWeather = function() {
   weather.find({search: 'Hungtingtown, MD', degreeType: 'F'}, function(err, result) {
-    if(err) console.log(err);
+    if(err)
+      console.log(err);
     curr = result[0].current.temperature;
     skytxt = result[0].current.skytext;
     var str = "It is " + curr + " degrees Fahrenheit and " + skytxt + " in Hungtingtown, MD";
